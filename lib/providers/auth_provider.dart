843c11e97afb6cccde82fb3ebbad0d837732e76f
Mahmoud Al Zahid <mah.draco.dev@gmail.com>
2025-08-08 09:35:17 +0200

    your message here

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import '../services/firebase_service.dart';
 import '../models/user_model.dart';
 
--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'dart:async';
 import '../services/firebase_service.dart';
 import '../models/user_model.dart';

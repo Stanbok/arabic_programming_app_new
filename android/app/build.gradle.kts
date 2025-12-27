921017794682d0a63d9123bb9074b5a757a97717
Mahmoud Al Zahid <mah.draco.dev@gmail.com>
2025-12-27 06:17:48 +0200

    your message here

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 android {
     namespace = "com.example.python_in_arabic"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
     ndkVersion = "27.0.12077973"
 
     compileOptions {

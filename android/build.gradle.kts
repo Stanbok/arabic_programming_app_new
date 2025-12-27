5863055dbee234b66e2a7d393cc7abcb3b872489
Mahmoud Al Zahid <mah.draco.dev@gmail.com>
2025-12-27 04:41:19 +0200

    your message here

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,4 +1,6 @@
 plugins {
+    id("com.android.application") version "8.12.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.0" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
 }
 
